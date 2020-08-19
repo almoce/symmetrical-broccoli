99c18bceb9bc6cfd2cc933eb43d7b6e5b85b4fe8
Wence <almo.ce@gmail.com>
2020-08-19 17:57:57 +0800

    feat: update webpack config file with config folder, specific dev.js for dev config

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+module.exports = (config) => {
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        disableHostCheck: true,
+        proxy: [
+            {
+                path: '/api/**',
+                target: 'http://192.168.0.124:1001'
+            }
+        ]
+    }
+
+}
---
+++ config/index.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename:'bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use:[
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [autoprefixer({})]
+                        }
+                    }]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODD_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
+}
